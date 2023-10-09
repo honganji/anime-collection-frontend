55c1553f7cfe10f2b39554989ad7a0c7da24d61f
github honganji <yujicavs@icloud.com>
2023-10-10 08:24:01 +0900

    complete login screen

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Login.css";
+import { useNavigate } from 'react-router-dom';
+import Input from '../components/parts/Input';
 
 export default function Lonin() {
+  const navigator = useNavigate();
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  const { name, email, password } = user;
+
+  function onInputChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+  async function onSubmit(e) {
+    e.preventDefault();
+    // await axios.post("http://localhost:8080/user", user);
+    navigator("/", {
+      state: {
+        name: name
+      }
+    });
+  };
   return (
-    <div id='login'>Lonin</div>
+    <div id='signup'>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <Input propertyName="name" value={name} onInputChange={onInputChange} />
+        {/* <Input propertyName="email" value={email} onInputChange={onInputChange} /> */}
+        <Input propertyName="password" value={password} onInputChange={onInputChange} />
+        <button type='submit' className='btn colored-btn'>Login</button>
+      </form>
+    </div>
   );
 }
