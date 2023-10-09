f5f8694728123209fc04dbda02a1e8c95b6455c4
github honganji <yujicavs@icloud.com>
2023-10-09 21:02:35 +0900

    modify body ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import "./Body.css";
 import AnimeItem from './parts/AnimeItem';
+import TabBlock from './parts/TabBlock';
 
 export default function Body() {
-  const animeTitleList = ["Jujutsu Kaisen", "Kingdom", "Demon Slayer", "Nisekoi"];
+
+  const animeTitleList = ["Jujutsu Kaisen", "Kingdom", "Demon Slayer", "Nisekoi", "Jujutsu Kaisen", "Kingdom", "Demon Slayer", "Nisekoi", "Demon Slayer", "Nisekoi"];
+  function getTabs() {
+    const numOfTabs = Math.floor(animeTitleList.length / 10);
+    if (numOfTabs === 0) {
+      return <>nothing</>;
+    }
+    return <TabBlock numOfTabs={numOfTabs} />;
+  }
+
   return (
     <div id='home-body'>
       <div className='title'>Anime List</div>
       {
         animeTitleList.map((animeTitle) => <AnimeItem />)
+      }
+      {
+        getTabs()
       }
     </div>
   );
---
+++ src/components/parts/TabBlock.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./TabBlock.css";
+
+export default function TabBlock(props) {
+  return (
+    <div id='tab-block'>{props.numOfTabs}</div>
+  );
+}
