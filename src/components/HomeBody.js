e6fe8b1788d3e0b83fd41400a9837ce4a5ba71e1
github honganji <yujicavs@icloud.com>
2023-10-11 08:42:58 +0900

    add data

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -9,10 +9,10 @@ export default function HomeBody(props) {
   const [params] = useSearchParams();
   const tabNum = params.get("tab") ?? 1;
   console.log(tabNum);
-  const dataList = props.animeList.slice((tabNum - 1) * 10, tabNum * 10);
+  const dataList = props.animeList.slice((tabNum - 1) * 7, tabNum * 7);
   console.log(dataList);
   function getTabs() {
-    const numOfTabs = Math.floor(animeList.length / 10 + 1);
+    const numOfTabs = Math.floor(animeList.length / 7 + 1);
     return <TabBlock numOfTabs={numOfTabs} />;
   }
 
