46858f116a53e2c0a7c4a29bd24ce32f4ca4a69f
honganji <85441887+honganji@users.noreply.github.com>
2023-11-13 12:58:57 +0900

    add validation function in login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,25 +4,18 @@ import { useNavigate } from 'react-router-dom';
 import Input from '../components/parts/Input';
 import { EXPIRE_SECOND, request, setAuthHeader } from '../helpers/axios_helpers';
 import Cookies from 'js-cookie';
+import { useForm } from "react-hook-form"
 
 export default function Login() {
   const navigator = useNavigate();
-  const [user, setUser] = useState({
-    emailAddress: "",
-    password: ""
-  });
-
-  const { emailAddress, password } = user;
-
-  function onInputChange(e) {
-    setUser({ ...user, [e.target.name]: e.target.value });
-  }
   async function onSubmit(e) {
-    e.preventDefault();
     const result = await request(
       "POST",
       "/login",
-      user
+      {
+        emailAddress: getValues("email"),
+        password: getValues("password")
+      }
     );
 
     setAuthHeader(result.data.token);
@@ -34,11 +27,64 @@ export default function Login() {
     Cookies.set('id', result.data.id, { expires: t });
     navigator("/");
   };
+
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm({
+    criteriaMode: 'all',
+  });
   return (
     <div id='login'>
-      <form onSubmit={(e) => onSubmit(e)}>
-        <Input propertyName="emailAddress" value={emailAddress} onInputChange={onInputChange} key="emailAddress" />
-        <Input propertyName="password" value={password} onInputChange={onInputChange} key="password" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div id="input">
+          <div className='item'>Email Address</div>
+          <input
+            className='box'
+            {...register('email', {
+              required: {
+                value: true,
+                message: '入力必須の項目',
+              },
+              pattern: {
+                value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: 'メールアドレスを入力してください',
+              },
+            })}
+          />
+        </div>
+        {errors.email?.types?.required && <div>入力が必須の項目です</div>}
+        {errors.email?.types?.pattern && <div>{errors.email?.types?.pattern}</div>}
+        {console.log(errors.email?.types)}
+        {console.log(getValues("email"))}
+
+        <div className="input">
+          <div className='item'>Password</div>
+          <input
+            className='box'
+            {...register('password', {
+              required: {
+                value: true,
+                message: 'You must input something',
+              },
+              minLength: {
+                value: 4,
+                message: 'Please make the length more than 4 characters',
+              },
+              maxLength: {
+                value: 20,
+                message: 'Please make the length less than 20 characters',
+              },
+            })}
+          />
+        </div>
+        {errors.password?.types?.required && <div>{errors.password?.types?.required}</div>}
+        {errors.password?.types?.minLength && <div>{errors.password?.types?.minLength}</div>}
+        {errors.password?.types?.maxLength && <div>{errors.password?.types?.maxLength}</div>}
+        {console.log(errors.password?.types)}
+        {console.log(getValues("password"))}
         <button type='submit' className='btn colored-btn'>Login</button>
       </form>
     </div>
